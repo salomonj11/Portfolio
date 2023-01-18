418a83df81638cded196c9203888915d7dbe3194
Jesus Salomon <salomonj1121@gmail.com>
2023-01-18 15:37:46 -0600

    added sanity to build

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -37,9 +37,6 @@ export default function Hero({}: Props) {
           <Link href="#about">
             <button className="heroButton">About</button>
           </Link>
-          <Link href="#experience">
-            <button className="heroButton">Experience</button>
-          </Link>
           <Link href="#skills">
             <button className="heroButton">Skills</button>
           </Link>
