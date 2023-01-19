38df333d040cf484f7ad951c6c3cb1d1b1563bce
Jesus Salomon <salomonj1121@gmail.com>
2023-01-19 14:21:05 -0600

    fixed up percentages on skills page

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -21,16 +21,54 @@ function Skills({}: Props) {
       </h3>
 
       <div className="grid grid-cols-4 gap-5 pt-20">
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
+        <Skill
+          imgSrc="https://www.logo.wine/a/logo/PostgreSQL/PostgreSQL-Logo.wine.svg"
+          progress={69}
+        />
+        <Skill
+          imgSrc="https://img.icons8.com/plasticine/512/react.png"
+          progress={67}
+        />
+        <Skill
+          imgSrc="https://img.icons8.com/color/512/mongodb.png"
+          progress={52}
+        />
+        <Skill
+          imgSrc="https://www.freepnglogos.com/uploads/javascript-png/javascript-logo-transparent-logo-javascript-images-3.png"
+          progress={81}
+        />
+        <Skill
+          imgSrc="https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_96102ac6497377cd53da621075fe828e/sanity.png"
+          progress={34}
+        />
+        <Skill
+          imgSrc="https://img.icons8.com/fluency/512/node-js.png"
+          progress={76}
+        />
+        <Skill
+          imgSrc="https://img.icons8.com/nolan/512/express-js.png"
+          progress={84}
+        />
+        <Skill
+          imgSrc="https://cdn.icon-icons.com/icons2/2107/PNG/512/file_type_typescript_icon_130108.png"
+          progress={38}
+        />
+        <Skill
+          imgSrc="https://cdn.icon-icons.com/icons2/2107/PNG/512/file_type_tailwind_icon_130128.png"
+          progress={46}
+        />
+        <Skill
+          imgSrc="https://cdn.thenewstack.io/media/2021/06/7c546588-nextjs.png"
+          progress={37}
+        />
+        <Skill
+          imgSrc="https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/512/external-html-5-is-a-software-solution-stack-that-defines-the-properties-and-behaviors-of-web-page-logo-shadow-tal-revivo.png"
+          progress={86}
+        />
+        <Skill
+          imgSrc="https://img.icons8.com/external-those-icons-lineal-color-those-icons/512/external-Git-social-media-those-icons-lineal-color-those-icons.png"
+          progress={84}
+        />
       </div>
     </motion.div>
   );
