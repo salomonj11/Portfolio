7dd54ea22b59d25c94745e1b0f6a4198ef2dfd1e
Jesus Salomon <salomonj1121@gmail.com>
2023-03-22 15:32:00 -0500

    added comments to workexperience component

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -5,8 +5,10 @@ import ExperienceCard from './ExperienceCard';
 type Props = {};
 
 function Experience({}: Props) {
+  // Set up state hook to hold the current window height
   const [windowHeight, setWindowHeight] = useState(0);
 
+  // Use an effect hook to update the window height on window resize
   useEffect(() => {
     function handleResize() {
       setWindowHeight(window.innerHeight);
@@ -20,8 +22,10 @@ function Experience({}: Props) {
     };
   }, []);
 
+  // Set a boolean variable to determine whether or not to display the text
   const shouldDisplayText = windowHeight > 900;
 
+  // Return the component with Framer Motion animation and ExperienceCard component
   return (
     <motion.div
       initial={{
@@ -31,6 +35,7 @@ function Experience({}: Props) {
       transition={{ duration: 1.5 }}
       className="h-screen flex relative overflow-hidden flex-col text-lft md:flex-row max-w-full px-10 justify-evenly mx-auto items-center"
     >
+      {/* Display the text if the window height is greater than 900 pixels aka laptops, otherwise don't display it. */}
       {shouldDisplayText && (
         <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
           Experience
